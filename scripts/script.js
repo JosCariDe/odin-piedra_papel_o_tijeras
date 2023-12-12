9dc7332986c87f4d144658b19586b354be7c2a7c
JoseCari <jdnarvaez@unimagdalena.edu.co>
2023-12-12 16:16:59 -0500

    Agregada la funcion para jugar una ronda Se hizo uso de UpperCase para aceptar cualquir formato para el usuario Se Agregó el mensaje para poder decidir el ganador Se eliminaron los consolo.log que estaban de pruebas en el anterior commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,24 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-const listaOpciones = ["Piedra" , "Papel" , "Tijeras"];
+const listaOpciones = ["PIEDRA" , "PAPEL" , "TIJERAS"];
 
 const getComputerChoice = () => {
     return listaOpciones[getRandomInt(3)];
 }
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+
+
+const playRound = (playerSelection , computerSelection) => {
+    let selectionPla = playerSelection.toUpperCase();
+    if( (selectionPla == listaOpciones[0] && computerSelection == listaOpciones[2]) ||
+        (selectionPla == listaOpciones[1] && computerSelection == listaOpciones[0]) ||
+        (selectionPla == listaOpciones[2] && computerSelection == listaOpciones[1]) ){
+            console.log("You win, " + selectionPla + " wins against " + computerSelection);
+    } else {
+        console.log("You lose, " + selectionPla + " loses against " + computerSelection);
+    }
+}
+
+console.log(playRound("papel",getComputerChoice()));
+
+
