1ec9818989a50d5939ffb3bd9e88d2f4b2ab2fc8
JoseCari <jdnarvaez@unimagdalena.edu.co>
2023-12-13 14:22:09 -0500

    Agregada la funcion para jugar 5 rondas Se solucionó el problema de los empates El retorno de la función de Round pasó a ser un String Se hizo uso de condicionales ternarios para mejorar en practica

--- scripts/script.js
+++ scripts/script.js
@@ -14,12 +14,43 @@ const playRound = (playerSelection , computerSelection) => {
     if( (selectionPla == listaOpciones[0] && computerSelection == listaOpciones[2]) ||
         (selectionPla == listaOpciones[1] && computerSelection == listaOpciones[0]) ||
         (selectionPla == listaOpciones[2] && computerSelection == listaOpciones[1]) ){
-            console.log("You win, " + selectionPla + " wins against " + computerSelection);
+            return "You win, " + selectionPla + " wins against " + computerSelection;
+    }else if (selectionPla == computerSelection) {
+        return "Empate, " + selectionPla + "empata contra " + computerSelection;
     } else {
-        console.log("You lose, " + selectionPla + " loses against " + computerSelection);
+        return "You lose, " + selectionPla + " loses against " + computerSelection;
     }
 }
 
-console.log(playRound("papel",getComputerChoice()));
+const game = () => {
+
+    let puntosJugador = 0;
+    let puntosMaquina = 0;
+
+    let opcionJugador;
+
+    let resultadoRonda;
+    
+    for (let i = 0; i < 5 ; i++) {
+
+        opcionJugador = prompt("Ingresa la Mano que desea jugar; piedra, papel o tijeras",null);
+
+        resultadoRonda = playRound(opcionJugador,getComputerChoice());
+
+        resultadoRonda[4] == "w" ? puntosJugador++ : 
+        resultadoRonda[4] == "l" ? puntosMaquina++ : null;
+
+        console.log(resultadoRonda);
+    }
+
+    let mensajeVictoriaFinal =  puntosJugador > puntosMaquina ? "El jungador ganó"  : 
+                                puntosMaquina > puntosJugador ? "La maquina ganó"   :
+                                "Empate, no hay ganador";
+
+    console.log(mensajeVictoriaFinal);
+}
+
+
+console.log(game());
 
 
